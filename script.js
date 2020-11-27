8aa2df9f761779801f467518144653c658aa05af
Tracey Saada <tracey.saada@outlook.com>
2020-11-27 17:53:21 +0000

    Add ajax call for current weather

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  var apiKey = "30ce5e2ebe1dfdf0be5f40518a98bd2a";
 $(".btn").on("click", function(){
  var searchedCity = $("#myInput").val(); 
 console.log(searchedCity)
@@ -8,7 +9,14 @@ console.log(searchedCity)
 })
 
 function searchForCityWeather(city){
-
+  $.ajax({
+    type: "GET",
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
+    dataType: "json",
+    success: function(dataReturned){
+      console.log("CityCurrentWeather", dataReturned);
+    }
+  })
 }
 
 
