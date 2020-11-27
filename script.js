0293da66e8001f2de55eece17776b4313da5f351
Tracey Saada <tracey.saada@outlook.com>
2020-11-27 18:09:58 +0000

    Set up forecast and uv calls

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ $(".btn").on("click", function(){
  var searchedCity = $("#myInput").val(); 
 console.log(searchedCity)
  searchForCityWeather(searchedCity);
-
 })
 
 function searchForCityWeather(city){
@@ -15,29 +14,34 @@ function searchForCityWeather(city){
     dataType: "json",
     success: function(dataReturned){
       console.log("CityCurrentWeather", dataReturned);
+
+
+      searchForecast(city); 
+      searchForUV(dataReturned.coord.lat, dataReturned.coord.lon);
     }
   })
 }
 
-
-})
-
-
-
-/*not sure of code
-  function searchIndexOf() {
+function searchForecast(city) {
+  $.ajax({
+    type: "GET",
+    url:`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`,
+    dataType: "json",
+    success: function(dataReturned){
+      console.log("CityForecast", dataReturned);
+    }
+    })
+  }
+  function searchForUV(lat, lon) {
     $.ajax({
       type: "GET",
-      url:"http://api.openweathermap.org/data/2.5/forecast?q={city}" + searchValue + "&appid=30ce5e2ebe1dfdf0be5f40518a98bd2a" 
-      }
-  
-  //get api id latitude & longitude
-    function search(lat, lon) {
-      $.ajax({
-        type: "GET",
-        url:"//http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}" + searchValue + "&appid=30ce5e2ebe1dfdf0be5f40518a98bd2a"   
-      }*/
-    
+      url:`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`,
+      dataType: "json",
+      success: function(dataReturned){
+        console.log("UVData", dataReturned)
+      } 
+    })
+
+  }     
       
-      
-    
+})
